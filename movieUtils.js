3b80eaeef55f394d2212d95be7f7eb594c7de70a
MICHAEL LIMACO <limaco.michael@gmail.com>
2025-04-04 17:49:34 -0500

    update lab 10

--- movieUtils.js
+++ movieUtils.js
@@ -42,4 +42,54 @@ function getRandomMovie(callback) {
         });
 }
 
-module.exports = { getMovieByTitle, getRandomMovie };
+function getAllMovies(callback, genre = null) {
+    const movies = [];
+
+    fs.createReadStream('data/movies.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+            const movie = {
+                id: row.id,
+                title: row.title,
+                year: row.year,
+                genre: row.genre,
+                director: row.director,
+                synopsis: row.plot
+            };
+
+            // Si hay un género especificado, filtra las películas
+            if (!genre || movie.genre.toLowerCase().includes(genre.toLowerCase())) {
+                movies.push(movie);
+            }
+        })
+        .on('end', () => {
+            callback(movies);
+        });
+}
+
+function getMovieByIdOrName(idOrName, callback) {
+    let movieFound = null;
+
+    fs.createReadStream('data/movies.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+            if (
+                row.id.toLowerCase() === idOrName.toLowerCase() ||
+                row.title.toLowerCase() === idOrName.toLowerCase()
+            ) {
+                movieFound = {
+                    id: row.id,
+                    title: row.title,
+                    year: row.year,
+                    genre: row.genre,
+                    director: row.director,
+                    synopsis: row.plot
+                };
+            }
+        })
+        .on('end', () => {
+            callback(movieFound);
+        });
+}
+
+module.exports = { getMovieByTitle, getRandomMovie, getAllMovies, getMovieByIdOrName };
